3b2ea09b5098caff5d48f0f11dfb5dd0949180b0
b03902042 <b03902042@linux10.csie.ntu.edu.tw>
2017-03-04 02:27:23 +0800

    pretty code

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,109 +1,109 @@
-import argparse
+import sys
 import pandas as pd
 import numpy as np
 
-def parse_arg():
-	parser = argparse.ArgumentParser(description='ML hw1: Linear Regression.')
-	parser.add_argument('-train', action='store', type=str, dest='train_data', required=True)
-	parser.add_argument('-epoch', action='store', type=int, dest='epoch', default=1000)
-	parser.add_argument('-lr', action='store', type=float, dest='lr', default=0.05)
-	parser.add_argument('-test', action='store', type=str, dest='test_data', required=True)
-	parser.add_argument('-output', action='store', type=str, dest='output', required=True)	
+def ReadTrainData(filename):
+    raw_data = pd.read_csv(filename, encoding='big5').as_matrix()
+    data = raw_data[:, 3:] # 12 months, 20 days per month, 18 features per day. shape: (4320 , 24)
+    data[data == 'NR'] = 0.0
+    data = data.astype('float')
 
-	return parser.parse_args()
+    X, Y = [], []
+    for i in range(0, data.shape[0], 18*20):
+        # i: start of each month
+        days = np.vsplit(data[i:i+18*20], 20) # shape: 20 * (18, 24)
+        concat = np.concatenate(days, axis=1) # shape: (18, 480)
+        for j in range(0, concat.shape[1]-10):
+            X.append(concat[:, j:j+9].flatten())
+            Y.append([concat[9, j+10]])
 
-def read_data(filename, istrain=True):
-	hr = 0
-	if istrain:
-		raw_data = pd.read_csv(filename, encoding='big5').as_matrix()
+    return np.array(X), np.array(Y)
 
-		_X = raw_data[:, 3:]
-		_X[_X == 'NR'] = 0.
-		_X = _X.astype('float')
+def ReadTestData(filename):
+    raw_data = pd.read_csv(filename, header=None, encoding='big5').as_matrix()
+    data = raw_data[:, 2:]
+    data[data == 'NR'] = 0.0
+    data = data.astype('float')
 
-		_X = _X.reshape(12, -1, 24)
-		_X = _X.reshape(12, -1, 18, 24)
-		_X = _X.swapaxes(1, 2).reshape(12, 18, -1)
+    obs = np.vsplit(data, data.shape[0]/18)
+    X = []
+    for i in obs:
+        X.append(i.flatten())
 
-		X, Y = [], []
-		for m in range(_X.shape[0]):
-			for i in range(0, _X.shape[2]-10):
-				X.append(_X[m, :, i+hr:i+9].flatten())
-				Y.append(_X[m, 9, i+10])
+    return np.array(X)
 
-		X, Y = np.asarray(X), np.asarray(Y)
-		return X, Y
-	else:
-		raw_data = pd.read_csv(filename, header=None, encoding='big5').as_matrix()
-		X = raw_data[:, 2+hr:]
-		X[X == 'NR'] = 0.
-		X = X.astype('float')
-		X = np.reshape(X, (-1, 18*(9-hr)))
-		return X
+class Linear_Regression():
+    def __init__(self):
+        pass
 
-def feature_scale(X, X_test):
-	for i in range(X.shape[1]):
-		# if i in range(90, 100):
-		# 	X[:, i] = np.square(X[:, i])
-		# 	X_test[:, i] = np.square(X_test[:, i])
-		tmp = X[:, i]
-		# M, m = tmp.max(), tmp.min()
-		# X[:, i] = (X[:, i] - m) / (M-m + 1e-20)
-		# X_test[:, i] = (X_test[:, i] - m) / (M-m + 1e-20)
+    def _error(self, X, Y):
+        return Y - self.predict(X)
 
-		mean, std = tmp.mean(), tmp.std()
-		X[:, i] = (X[:, i] - mean) / std
-		X_test[:, i] = (X_test[:, i] - mean) / (std + 1e-20)
+    def _loss(self, X, Y):
+        return np.sqrt(np.mean(np.square(self._error(X, Y))))
 
-	return X, X_test
+    def _init_parameters(self):
+        self.B = np.random.rand() - 0.5
+        self.W = np.random.rand(self.feature_dim, 1) - 0.5
 
-def linear_regression(X, Y, lr, max_epoch):
-	N = X.shape[0] # number of data
+    def _scale(self, X, istrain=True):
+        if istrain:
+            self.max = np.max(X, axis=0)
+            self.min = np.min(X, axis=0)
+        return (X - self.min) / (self.max - self.min)
 
-	P = X.shape[1] # number of features
-	W = np.random.rand(P, 1) - 0.5 # weight matrix
+    def fit(self, _X, Y, max_epoch=100000, lr=0.1):
+        assert _X.shape[0] == Y.shape[0]
+        N = _X.shape[0]
+        self.feature_dim = feature_dim = _X.shape[1]
 
-	print('Number of data: ', N)
-	print('Number of features: ', P)
+        X = self._scale(_X)
 
-	W_lr = np.zeros((P, 1))
-	for epoch in range(1, max_epoch+1):
-		H = predict(W, X).flatten()
-		diff = H - Y
-		loss = np.sqrt((1. / N) * np.sum(np.square(diff)))
-		if epoch % 100 == 0:
-			print('Epoch: {}, loss: {}'.format(epoch, loss))
+        self._init_parameters()
 
-		dW = np.zeros((P, ))
-		for i in range(N):
-			dW = dW + diff[i] * X[i, :]
+        B_lr = 1e-20
+        W_lr = np.full((feature_dim, 1), 1e-20)
+        for epoch in range(1, max_epoch+1):
 
-		dW = 2 * dW.reshape((P, 1))
-		W_lr = W_lr + np.square(dW)
-		W = W - lr / np.sqrt(W_lr) * dW
-		# W = W - lr * dW
+            B_grad = np.random.rand() - 0.5
+            W_grad = np.random.rand(feature_dim, 1) - 0.5
 
-	return W
+            error = self._error(X, Y)
 
-def predict(W, X):
-	return np.dot(X, W)
+            B_grad = B_grad - 2.0 * np.sum(error) * 1.0
+            W_grad = W_grad - 2.0 * np.dot(X.T, error)
+
+            B_lr = B_lr + B_grad ** 2
+            W_lr = W_lr + W_grad ** 2
+
+            self.B = self.B - lr / np.sqrt(B_lr) * B_grad
+            self.W = self.W - lr / np.sqrt(W_lr) * W_grad
+
+            if epoch % 100 == 0:
+                print('[Epoch {}]: loss: {}'.format(epoch, self._loss(X, Y)))
+
+    def predict(self, X):
+        _X = X.reshape((-1, self.feature_dim))
+        return np.dot(_X, self.W) + self.B
+ 
+    def predict_test(self, X):
+        _X = self._scale(X, istrain=False)
+        _X = _X.reshape((-1, self.feature_dim))
+        return np.dot(_X, self.W) + self.B
 
 def main(args):
-	X, Y = read_data(args.train_data)
-	X_test = read_data(args.test_data, istrain=False)
+    X, Y = ReadTrainData(args[1])
 
-	X, X_test = feature_scale(X, X_test)
-	X = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1) # concat ones
-	X_test = np.concatenate((np.ones((X_test.shape[0], 1)), X_test), axis=1) # concat ones
+    model = Linear_Regression()
+    model.fit(X, Y, max_epoch=100000, lr=10)
 
-	W = linear_regression(X, Y, args.lr, args.epoch)
-	H_test = predict(W, X_test)
+    X_test = ReadTestData(args[2])
+    predict = model.predict_test(X_test)
 
-	with open(args.output, 'w') as f:
-		print('id,value', file=f)
-		for (i, v) in enumerate(H_test):
-			print('id_{},{}'.format(i, v[0]), file=f)
+    with open(args[3], 'w') as f:
+        print('id,value', file=f)
+        for (i, p) in enumerate(predict) :
+            print('id_{},{}'.format(i, p[0]), file=f)
 
 if __name__ == '__main__':
-	args = parse_arg()
-	main(args)
+    main(sys.argv)
