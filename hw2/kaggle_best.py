c195f524f76c343709bfa3207f07598cb57e0803
WindQAQ <b03902042@linux1.csie.ntu.edu.tw>
2017-04-01 23:31:06 +0800

    public score: 0.87273

--- hw2/kaggle_best.py
+++ hw2/kaggle_best.py
@@ -1,9 +1,147 @@
 import sys
-import numpy as np
 import pandas as pd
+import numpy as np
+import pickle
 
-def sigmoid(X):
-    return 1 / (1 + np.exp(-X))
+class DecisionNode():
+    def __init__(self, attr=None, thres=None, left=None, right=None, pred=None):
+        self.attr = attr
+        self.thres = thres
+        self.left = left
+        self.right = right
+        self.pred = pred
+
+class DecisionTree():
+    def __init__(self, min_samples_split=2, min_impurity=1e-7, max_depth=float('inf')):
+        self.root = None
+        self.min_samples_split = min_samples_split
+        self.min_impurity = min_impurity
+        self.max_depth = max_depth
+        self._impurity_on_node = None
+        self._impurity = None
+        self._leaf_pred = None
+
+    def fit(self, X, Y):
+        self.root = self._build(X, Y)
+
+    def _build(self, X, Y, depth=0):
+        min_impurity = init_impurity = 1e20
+        best_criteria = None
+
+        n_samples, n_features = X.shape
+
+        size = len(Y)
+        if n_samples >= self.min_samples_split and depth <= self.max_depth:
+            for attr in range(n_features):
+                sorted_indices = np.argsort(X[:, attr])
+                X, Y = X[sorted_indices], Y[sorted_indices]
+
+                for i in range(0, n_samples-1):
+                    if X[i, attr] < X[i+1, attr]:
+                        impurity = (
+                                self._impurity_on_node(Y[:i+1]) * (i+1) + 
+                                self._impurity_on_node(Y[i+1:]) * (size-i-1)
+                                ) / size
+                        if impurity < min_impurity:
+                            thres = (X[i, attr] + X[i+1, attr]) / 2
+                            min_impurity = impurity
+                            best_criteria = (attr, thres)
+
+        impurity_on_node = self._impurity_on_node(Y)
+        if  impurity_on_node < self.min_impurity or min_impurity == init_impurity or depth > self.max_depth:
+            pred = self._leaf_pred(Y)
+            print('predict {} with impurity {}'.format(pred, impurity_on_node))
+            return DecisionNode(pred=pred)
+        else:
+            attr, thres = best_criteria
+            print('split at attr {} with threshold {}'.format(attr, thres))
+            l, r = (X[:, attr] < thres), (X[:, attr] >= thres)
+            X_l, Y_l, X_r, Y_r = X[l], Y[l], X[r], Y[r]
+            left = self._build(X_l, Y_l, depth=depth+1)
+            right = self._build(X_r, Y_r, depth=depth+1)
+            return DecisionNode(attr=attr, thres=thres, left=left, right=right)
+
+    def predict(self, X):
+        pred = []
+        for x in X:
+            pred.append(self._predict_one(x, node=self.root))
+        return np.array(pred)
+
+    def _predict_one(self, x, node=None):
+        if node.pred is not None:
+            return node.pred
+
+        f = x[node.attr]
+        child = node.left
+        if f >= node.thres:
+            child = node.right
+
+        return self._predict_one(x, node=child)
+
+class RegressionTree(DecisionTree):
+    def _cal_var(self, Y):
+        mean = np.ones(np.shape(Y)) * Y.mean(0)
+        n_samples = np.shape(Y)[0]
+        var = (1 / n_samples) * np.diag((Y-mean).T.dot(Y-mean))
+        return var
+
+    def _variance_reduce(self, Y, Y_l, Y_r):
+        var = self._cal_var(Y)
+        var_l = self._cal_var(Y_l)
+        var_r = self._cal_var(Y_r)
+      
+        return np.sum(var - var_l * (len(Y_l)/len(Y)) - var_r * (len(Y_r)/len(Y)))
+
+    def _variance_on_node(self, Y):
+        return np.sum(self._cal_var(Y))
+
+    def _mean(self, Y):
+        return np.mean(Y, axis=0)
+
+    def fit(self, X, Y):
+        self._impurity_on_node = self._variance_on_node
+        self._impurity = self._variance_reduce
+        self._leaf_pred = self._mean
+        super(RegressionTree, self).fit(X, Y)
+
+class GradientBoostClassifier():
+    def __init__(self, n_estimators=20, learning_rate=0.1, 
+            max_depth=3, min_samples_split=2, min_impurity=1e-7):
+
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_impurity = min_impurity
+
+        self.estimators = []
+        for i in range(self.n_estimators):
+            est = RegressionTree(
+                    min_samples_split=self.min_samples_split,
+                    min_impurity=self.min_impurity,
+                    max_depth=self.max_depth)
+            self.estimators.append(est)
+
+    def fit(self, X, Y):
+        pred = np.full(np.shape(Y), np.mean(Y, axis=0))
+        for i, est in enumerate(self.estimators):
+            print('build tree {}'.format(i))
+            grad = self._gradient(Y, pred)
+            est.fit(X, grad)
+            update = est.predict(X)
+
+            pred -= self.learning_rate * update
+
+    def predict(self, X):
+        pred = 0.0
+        for est in self.estimators:
+            update = est.predict(X)
+            pred -= self.learning_rate * update
+
+        return np.argmax(pred, axis=1)
+
+    def _gradient(self, y, pred):
+        return -(y - 1. / (1. + np.exp(-pred)))
 
 def read_data(filename, label=False):
     if label:
@@ -13,144 +151,55 @@ def read_data(filename, label=False):
 
     return data.as_matrix().astype('float')
     
-class Logistic_Regression():
-    def __init__(self):
-        pass
+def one_hot(Y):
+    _Y = np.zeros((Y.shape[0], 2))
+    _Y[np.arange(Y.shape[0]), Y.flatten()] = 1
 
-    def _init_para(self):
-        self.W = np.zeros((self.feature_dim + 1, 1))
-        self._W_lr = 0.0
-
-    def fit(self, X, Y, valid=None, max_epochs=2000, lr=0.05, C=0.0):
-        assert X.shape[0] == Y.shape[0]
-        self.feature_dim = X.shape[1]
-        self._init_lr = self._lr = lr
-        self.C = C
-
-        self._init_para()
-        
-        self._mean, self._std = self._get_attr(X)
-        X = self._scale(X)
-        X = self._add_bias(X)
-        if valid is not None:
-            X_valid = self._scale(valid[0])
-            X_valid = self._add_bias(X_valid)
-            Y_valid = valid[1]
-
-        for epoch in range(1, max_epochs+1):
-            self._step(X, Y)
-
-            if epoch % 100 == 0:
-                loss = self._loss(X, Y)
-                acc = self.evaluate(X, Y)
-                print('[Epoch {:5d}] - training loss: {:.5f}, accuracy: {:.5f}'.format(epoch, loss, acc))
-                if valid is not None:
-                    print('\tvalid loss: {:.5f}, accuracy: {:.5f}'.format(self._loss(X_valid, Y_valid), self.evaluate(X_valid, Y_valid)))
-
-    def predict(self, X, test=False):
-        # sigmoid(X * W)
-        if test:
-            X = self._scale(X)
-            X = self._add_bias(X)
-        return sigmoid(np.dot(X, self.W))
-
-    def _get_attr(self, X):
-        return np.mean(X, axis=0), np.std(X, axis=0) 
-
-    def _scale(self, X):
-        return (X - self._mean) / (self._std + 1e-20)
-
-    def _step(self, X, Y):
-        pred = self.predict(X)
-        self._update(X, Y, pred)
-
-    def _update(self, X, Y, pred):
-        grad = self._gradient(X, Y, pred)
-
-        self._W_lr = self._W_lr + grad ** 2
-        self._lr = self._init_lr / np.sqrt(self._W_lr)
-
-        self.W = self.W - self._lr * (grad + self.C * np.sum(self.W))
-
-    def _gradient(self, X, Y, pred):
-        return -np.dot(X.T, (Y - pred))
-
-    def _loss(self, X, Y, pred=None):
-        # y_hat: prediction of model
-        # -mean(y * log(y_hat) + ((1 - y) * log((1-y_hat))))
-        if pred is None:
-            pred = self.predict(X)
-        return -np.mean(Y * np.log(pred + 1e-20) + (1 - Y) * np.log((1 - pred + 1e-20)))
-
-    def evaluate(self, X, Y, test=False):
-        if test:
-            X = self._scale(X)
-            X = self._add_bias(X)
-        pred = self.predict(X)
-        p = pred
-        p[pred < 0.5] = 0.0
-        p[pred >= 0.5] = 1.0
-        return np.mean(1 - np.abs(Y - p))
-
-    def _add_bias(self, X):
-        return np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
+    return _Y
 
 def main(args):
     X = read_data(args[1])
-    Y = read_data(args[2], label=True)    
+    Y = read_data(args[2], label=True).astype('int') 
     X_test = read_data(args[3])
 
-    square = [0, 1, 3, 4, 5]
-    cubic = [0, 1, 3, 4, 5]
-    four = [0, 1, 3, 4, 5]
-    five = [0, 1, 3, 4, 5]
-
-    X = np.concatenate((
-                X, 
-                X[:, square] ** 2,
-                X[:, cubic] ** 3, 
-                X[:, four] ** 4, 
-                X[:, five] ** 5,
-                np.log(X[:, [0, 1, 3, 4, 5]] + 1e-10),
-                (X[:, 0] * X[:, 3]).reshape((-1, 1)),
-                (X[:, 0] * X[:, 5]).reshape((-1, 1)),
-                (X[:, 0] * X[:, 5]).reshape((-1, 1)) ** 2,
-                (X[:, 3] * X[:, 5]).reshape((-1, 1)),
-                X[:, 6:] * X[:, 5].reshape((-1, 1)),
-                (X[:, 3] - X[:, 4]).reshape((-1, 1)),
-                (X[:, 3] - X[:, 4]).reshape((-1, 1)) ** 3
-            ), axis=1)
-
-    X_test = np.concatenate((
-                X_test, 
-                X_test[:, square] ** 2,
-                X_test[:, cubic] ** 3,
-                X_test[:, four] ** 4,
-                X_test[:, five] ** 5,
-                np.log(X_test[:, [0, 1, 3, 4, 5]] + 1e-10),
-                (X_test[:, 0] * X_test[:, 3]).reshape(-1, 1),
-                (X_test[:, 0] * X_test[:, 5]).reshape(-1, 1),
-                (X_test[:, 0] * X_test[:, 5]).reshape(-1, 1) ** 2,
-                (X_test[:, 3] * X_test[:, 5]).reshape(-1, 1),
-                X_test[:, 6:] * X_test[:, 5].reshape((-1, 1)),
-                (X_test[:, 3] - X_test[:, 4]).reshape((-1, 1)),
-                (X_test[:, 3] - X_test[:, 4]).reshape((-1, 1)) ** 3
-            ), axis=1)
+    Y = one_hot(Y)
 
     valid = None
-    if len(args) == 6:
+    if len(args) >= 6:
         valid_num = int(args[5])
         X_train, Y_train = X[:-valid_num], Y[:-valid_num]
         valid = (X[-valid_num:], Y[-valid_num:])
     else:
         X_train, Y_train = X, Y
 
-    model = Logistic_Regression()
-    model.fit(X_train, Y_train, valid=valid, C=0.0, max_epochs=3000, lr=0.05)
+    if len(args) == 7:
+        # load model
+        print('load model, path {}'.format(args[6]))
+        with open(args[6], 'rb') as fmodel:
+            model = pickle.load(fmodel)
+    else:
+        # train model
+        model = GradientBoostClassifier(n_estimators=250, learning_rate=0.5, max_depth=4)
+        model.fit(X_train, Y_train)
+
+    try:
+        pred = model.predict(X_train)
+        train_acc = np.mean(pred.flatten() == np.argmax(Y_train, axis=1).flatten())
+        print('training accuracy: {}'.format(train_acc))
+        if valid is not None:
+            pred = model.predict(valid[0])
+            valid_acc = np.mean(pred.flatten() == np.argmax(valid[1], axis=1).flatten())
+            print('valid accuracy: {}'.format(valid_acc))
+    except:
+        pass
+
+    if len(args) < 7:
+        with open('model', 'wb') as fmodel:
+            pickle.dump(model, fmodel, protocol=pickle.HIGHEST_PROTOCOL)
 
     with open(args[4], 'w') as fout:
         print('id,label', file=fout)
-        pred = model.predict(X_test, test=True)
+        pred = model.predict(X_test)
         for (i, v) in enumerate(pred.flatten()):
             print('{},{}'.format(i+1, 1 if v >= 0.5 else 0), file=fout)
 
