e103ad671870707ee64011e731da1e6dadbf34a6
WindQAQ <b03902042@linux1.csie.ntu.edu.tw>
2017-04-03 17:04:07 +0800

    public score: 0.85995

--- hw2/hw2_logistic.sh
+++ hw2/hw2_logistic.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3.6 logistic_regression.py $3 $4 $5 $6
+python3.6 logistic_regression.py $3 $4 $5 $6 6000
--- hw2/logistic_regression.py
+++ hw2/logistic_regression.py
@@ -10,10 +10,6 @@ def read_data(filename, label=False):
         data = pd.read_csv(filename, header=None)
     else:
         data = pd.read_csv(filename)
-        data['square_hours'] = data['hours_per_week'] ** 2
-        data['square_gain'] = data['capital_gain'] ** 2
-        data['square_loss'] = data['capital_loss'] ** 2
-        data['cubic_hours'] = data['hours_per_week'] ** 3
 
     return data.as_matrix().astype('float')
     
@@ -25,7 +21,7 @@ class Logistic_Regression():
         self.W = np.zeros((self.feature_dim + 1, 1))
         self._W_lr = 0.0
 
-    def fit(self, X, Y, max_epochs=2000, lr=0.05, C=0.0):
+    def fit(self, X, Y, valid=None, max_epochs=2000, lr=0.05, C=0.0):
         assert X.shape[0] == Y.shape[0]
         self.feature_dim = X.shape[1]
         self._init_lr = self._lr = lr
@@ -36,6 +32,10 @@ class Logistic_Regression():
         self._mean, self._std = self._get_attr(X)
         X = self._scale(X)
         X = self._add_bias(X)
+        if valid is not None:
+            X_valid = self._scale(valid[0])
+            X_valid = self._add_bias(X_valid)
+            Y_valid = valid[1]
 
         for epoch in range(1, max_epochs+1):
             self._step(X, Y)
@@ -44,6 +44,8 @@ class Logistic_Regression():
                 loss = self._loss(X, Y)
                 acc = self.evaluate(X, Y)
                 print('[Epoch {:5d}] - training loss: {:.5f}, accuracy: {:.5f}'.format(epoch, loss, acc))
+                if valid is not None:
+                    print('\tvalid loss: {:.5f}, accuracy: {:.5f}'.format(self._loss(X_valid, Y_valid), self.evaluate(X_valid, Y_valid)))
 
     def predict(self, X, test=False):
         # sigmoid(X * W)
@@ -80,7 +82,10 @@ class Logistic_Regression():
             pred = self.predict(X)
         return -np.mean(Y * np.log(pred + 1e-20) + (1 - Y) * np.log((1 - pred + 1e-20)))
 
-    def evaluate(self, X, Y):
+    def evaluate(self, X, Y, test=False):
+        if test:
+            X = self._scale(X)
+            X = self._add_bias(X)
         pred = self.predict(X)
         p = pred
         p[pred < 0.5] = 0.0
@@ -95,8 +100,53 @@ def main(args):
     Y = read_data(args[2], label=True)    
     X_test = read_data(args[3])
 
+    square = [0, 1, 3, 4, 5]
+    cubic = [0, 1, 3, 4, 5]
+    four = [0, 1, 3, 4, 5]
+    five = [0, 1, 3, 4, 5]
+
+    X = np.concatenate((
+                X, 
+                X[:, square] ** 2,
+                X[:, cubic] ** 3, 
+                X[:, four] ** 4, 
+                X[:, five] ** 5,
+                np.log(X[:, [0, 1, 3, 4, 5]] + 1e-10),
+                (X[:, 0] * X[:, 3]).reshape((-1, 1)),
+                (X[:, 0] * X[:, 5]).reshape((-1, 1)),
+                (X[:, 0] * X[:, 5]).reshape((-1, 1)) ** 2,
+                (X[:, 3] * X[:, 5]).reshape((-1, 1)),
+                X[:, 6:] * X[:, 5].reshape((-1, 1)),
+                (X[:, 3] - X[:, 4]).reshape((-1, 1)),
+                (X[:, 3] - X[:, 4]).reshape((-1, 1)) ** 3
+            ), axis=1)
+
+    X_test = np.concatenate((
+                X_test, 
+                X_test[:, square] ** 2,
+                X_test[:, cubic] ** 3,
+                X_test[:, four] ** 4,
+                X_test[:, five] ** 5,
+                np.log(X_test[:, [0, 1, 3, 4, 5]] + 1e-10),
+                (X_test[:, 0] * X_test[:, 3]).reshape(-1, 1),
+                (X_test[:, 0] * X_test[:, 5]).reshape(-1, 1),
+                (X_test[:, 0] * X_test[:, 5]).reshape(-1, 1) ** 2,
+                (X_test[:, 3] * X_test[:, 5]).reshape(-1, 1),
+                X_test[:, 6:] * X_test[:, 5].reshape((-1, 1)),
+                (X_test[:, 3] - X_test[:, 4]).reshape((-1, 1)),
+                (X_test[:, 3] - X_test[:, 4]).reshape((-1, 1)) ** 3
+            ), axis=1)
+
+    valid = None
+    if len(args) == 6:
+        valid_num = int(args[5])
+        X_train, Y_train = X[:-valid_num], Y[:-valid_num]
+        valid = (X[-valid_num:], Y[-valid_num:])
+    else:
+        X_train, Y_train = X, Y
+
     model = Logistic_Regression()
-    model.fit(X, Y, C=0.0, max_epochs=2000, lr=0.05)
+    model.fit(X_train, Y_train, valid=valid, C=0.0, max_epochs=3000, lr=0.05)
 
     with open(args[4], 'w') as fout:
         print('id,label', file=fout)
