bb28f7602c83a28714a13b0205b318c40814743a
WindQAQ <b03902042@ntu.edu.tw>
2017-04-12 01:44:46 +0800

    public score: 0.62274

--- hw3/train.py
+++ hw3/train.py
@@ -43,13 +43,15 @@ def main(args):
     model.add(Conv2D(32, kernel_size=(3, 3), input_shape=input_shape, activation='relu'))
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(3, 3)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
     model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
     model.add(AveragePooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Flatten())
 
     model.add(Dense(128, activation='relu'))
