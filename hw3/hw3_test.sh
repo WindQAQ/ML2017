6672b7bfa444bee34ececb53a3ba7ce80fd78cca
WindQAQ <b03902042@ntu.edu.tw>
2017-04-30 13:21:53 +0800

    public score: 0.72722

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3.6 test.py $1 $2 attr.npy model/model1.h5 model/model2.h5 model/model3.h5
+python3.6 test.py $1 $2 attr.npy model/model1.h5 model/model2.h5 model/model3.h5 model/model4.h5
