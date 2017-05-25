681473a842f10a3781f7b405f12d7fb2d0e2ec29
WindQAQ <b03902042@ntu.edu.tw>
2017-05-25 13:24:15 +0800

    set seed

--- hw5/train.py
+++ hw5/train.py
@@ -56,29 +56,25 @@ class ArticleClassifier():
             trainable=False))
 
         ''' 
-        model.add(Conv1D(filters=64, kernel_size=5, padding='same'))
-        model.add(LeakyReLU())
-        model.add(BatchNormalization())
+        model.add(Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'))
         model.add(Dropout(0.3))
-        model.add(Conv1D(filters=64, kernel_size=5, padding='same'))
-        model.add(LeakyReLU())
-        model.add(BatchNormalization())
+        model.add(Conv1D(filters=128, kernel_size=3, padding='same', activation='relu'))
         model.add(Dropout(0.3))
         #model.add(AveragePooling1D(pool_size=2, padding='same'))
         '''
 
-        for i in range(num_layer):
-            model.add(GRU(rnn_size, activation='tanh', return_sequences=(i != num_layer-1), dropout=0.3))
+        for i, size in enumerate(rnn_size):
+            model.add(GRU(size, activation='tanh', return_sequences=(i != len(rnn_size)-1), dropout=0.3))
         
-        model.add(Dense(256, activation='relu'))
-        model.add(Dropout(0.3))    
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.5))    
 
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.5))
+       
         model.add(Dense(128, activation='relu'))
-        model.add(Dropout(0.3))
-        
-        model.add(Dense(64, activation='relu'))
-        model.add(Dropout(0.3))
-        
+        model.add(Dropout(0.5))
+
         model.add(Dense(self.num_classes, activation='sigmoid'))
         self.model = model
 
@@ -127,13 +123,18 @@ def read_data(filename, isTrain=True):
 
 
 @print_func
-def preprocess(train, test, maxlen=400, split=" ", filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\'\t\n'):
-    tokenizer = Tokenizer(filters=filters, split=split)
-    tokenizer.fit_on_texts(train + test)
-    sequences_train = tokenizer.texts_to_sequences(train)
-    sequences_test = tokenizer.texts_to_sequences(test)
-    tokenizer.word_index['<PAD>'] = 0
-    return pad_sequences(sequences_train, maxlen=maxlen), pad_sequences(sequences_test, maxlen=maxlen), tokenizer
+def preprocess(train, test, tokenizer=None, maxlen=400, split=" ", filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\'\t\n'):
+    if tokenizer is None:
+        tokenizer = Tokenizer(filters=filters, split=split)
+        tokenizer.fit_on_texts(train + test)
+        sequences_train = tokenizer.texts_to_sequences(train)
+        sequences_test = tokenizer.texts_to_sequences(test)
+        tokenizer.word_index['<PAD>'] = 0
+        return pad_sequences(sequences_train, maxlen=maxlen), pad_sequences(sequences_test, maxlen=maxlen), tokenizer
+    else:
+        sequences_train = tokenizer.texts_to_sequences(train)
+        sequences_test = tokenizer.texts_to_sequences(test)
+        return pad_sequences(sequences_train, maxlen=maxlen), pad_sequences(sequences_test, maxlen=maxlen)
 
 
 @print_func
@@ -142,6 +143,8 @@ def parse_args():
     parser.add_argument('--train')
     parser.add_argument('--test')
     parser.add_argument('--embedding')
+    parser.add_argument('--tokenizer')
+    parser.add_argument('--mlb')
     return parser.parse_args()
 
 
@@ -162,22 +165,33 @@ def main(args):
     if args.train is not None:
         texts_train, labels = read_data(args.train)
         texts_test = read_data(args.test, isTrain=False)
-        sequences, _, tokenizer = preprocess(texts_train, texts_test)
+        if args.tokenizer is None:
+            sequences, _, tokenizer = preprocess(texts_train, texts_test)
+        else:
+            print('load tokenizer')
+            tokenizer = pickle.load(open(args.tokenizer, 'rb'))
+            sequences, _ = preprocess(texts_train, texts_test, tokenizer=tokenizer)
+
         embedding = read_embedding(args.embedding, tokenizer.word_index)
 
-        mlb = MultiLabelBinarizer()
+        if args.mlb is None:
+            mlb = MultiLabelBinarizer()
+        else:
+            print('load mlb')
+            mlb = pickle.load(open(args.mlb, 'rb'))
+
         labels = mlb.fit_transform(labels)
 
-
-        for i in range(5, 10):
+        for i in range(10, 11):
             model = ArticleClassifier(tokenizer=tokenizer, mlb=mlb, embedding=embedding)
 
-            model.build(num_layer=1, rnn_size=128)
+            model.build(rnn_size=[128, 128, 128])
             model.compile()
             model.summary()
 
-            callbacks = [EarlyStopping(monitor='val_fmeasure', patience=10, verbose=1, mode='max'),
-                    ModelCheckpoint('model-{}.h5'.format(i), monitor='val_fmeasure', verbose=1, save_best_only=True, mode='max')]
+            callbacks = [] 
+            callbacks.append(EarlyStopping(monitor='val_fmeasure', patience=25, verbose=1, mode='max'))
+            callbacks.append(ModelCheckpoint('model-{}.h5'.format(i), monitor='val_fmeasure', verbose=1, save_best_only=True, mode='max'))
            
             np.random.seed(i)
             order = np.random.permutation(len(sequences))
@@ -187,7 +201,12 @@ def main(args):
             sequences_valid, labels_valid = sequences[:490], labels[:490]
 
             model.fit(sequences_train, labels_train, epochs=50000, batch_size=128, validation_data=(sequences_valid, labels_valid), callbacks=callbacks)
-            model.save()
+        
+        if args.tokenizer is None or args.mlb is None:
+            print('save misc')
+            pickle.dump(tokenizer, open('word_index', 'wb'))
+            pickle.dump(mlb, open('label_mapping', 'wb'))
+
 
 if __name__ == '__main__':
     args = parse_args()
