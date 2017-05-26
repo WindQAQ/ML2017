2565e16bfb0ca051c1260f9e997ee272258c2735
WindQAQ <b03902042@ntu.edu.tw>
2017-05-26 20:19:36 +0800

    trivial

--- hw5/hw5_best.sh
+++ hw5/hw5_best.sh
@@ -2,7 +2,7 @@
 
 python3 predict.py --model best/model-5.h5 best/model-5-o.h5 best/model-9.h5 best/model-10.h5 \
                    --default_pred best/model-10.h5 \
-                   --train data/train_data.csv \
+                   --train best/model \
                    --tokenizer best/word_index \
                    --mlb best/label_mapping \
                    --tfidf \
--- hw5/hw5_rnn.sh
+++ hw5/hw5_rnn.sh
@@ -2,7 +2,7 @@
 
 python3 predict.py --model best/model-5.h5 \
                    --default_pred best/model-5.h5 \
-                   --train data/train_data.csv \
+                   --train best/model \
                    --tokenizer best/word_index \
                    --mlb best/label_mapping \
                    --test $1 \
