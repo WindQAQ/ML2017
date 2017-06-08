f2000c0c98eabedd1435198eb853c62f7e451ef3
WindQAQ <b03902042@ntu.edu.tw>
2017-06-09 01:06:31 +0800

    split to slices

--- hw6/hw6_best.sh
+++ hw6/hw6_best.sh
@@ -1,3 +1,6 @@
 #/usr/bin/env bash
 
+cat best/dual-svdpp-model-14-* > best/dual-svdpp-model-14.h5
+cat best/dual-svdpp-model-5-* > best/dual-svdpp-model-5.h5
+
 python3.6 predict.py --test $1/test.csv --output $2 --user2id best/user2id.npy --movie2id best/movie2id.npy --model best/dual-svdpp-model-14.h5 best/dual-svdpp-model-5.h5
